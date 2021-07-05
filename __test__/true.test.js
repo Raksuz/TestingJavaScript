bff25f93f8009abb80cbef5d12fa2084a30b15fc
Carlos Lopez <carsus.1112@gmail.com>
2021-07-05 11:28:27 -0500

    Probando valores verdaderos, flasos, null, undefined video 2.3

---
+++ __test__/true.test.js
@@ -0,0 +1,30 @@
+import { expect } from '@jest/globals';
+import { test } from 'jest-circus';
+import { describe } from 'yargs';
+import {isNull, isTrue, isFalse, isUndefined} from '../true';
+
+describe('Probar resultados nulos', () => {
+    test('null', () => {
+        expect(isNull()).toBeNull();
+    });
+});
+describe('Probar resultados verdaderos', () => {
+    test('Verdadero', () => {
+        expect(isTrue()).toBeTruthy();
+    });
+});
+describe('Probar resultados falsos', () => {
+    test('falso', () => {
+        expect(isFalse()).toBeFalsy();
+    });
+});
+describe('Probar resultados undefined', () => {
+    test('Indefinidos', () => {
+        expect(isUndefined()).toBeUndefined();
+    });
+});
+describe('Probar resultados no verdaderos', () => {
+    test('Falso o Verdadero', () => {
+        expect(isFalse()).not.toBeTruthy();
+    });
+});
