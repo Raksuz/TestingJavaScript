ad894fe85f28547ebcbf5f40ba380396d8df512b
Carlos Lopez <carsus.1112@gmail.com>
2021-07-06 12:43:07 -0500

    Agregando excepciones a snapshots video 4.2

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,20 @@ describe('Es hora de las instantanias', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     });
+    test('Siempre fallara la instantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+        }
+        expect(user).toMatchSnapshot();
+    });
+    test('Tenemos una excepcion dentro del codigo', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "Carlos Lopez Chacon"
+        }
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        });
+    });
 });
